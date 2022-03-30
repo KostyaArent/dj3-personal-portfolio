edf283a61d04913e335f208d4b55f7f574e8f9e3
KostyaArent <trepalin.konstantin@gmail.com>
2022-03-30 18:20:57 +0000

    blog model added

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=60)
+    publish_date = models.DateField(auto_now_add=True)
+    description = models.TextField()
