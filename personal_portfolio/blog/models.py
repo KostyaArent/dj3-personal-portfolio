3502efe48a030a9f4e4cecf367802ebc0d2cb6bf
KostyaArent <trepalin.konstantin@gmail.com>
2022-03-30 20:30:06 +0000

    blog template structure added

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Blog(models.Model):
     title = models.CharField(max_length=60)
     publish_date = models.DateField(auto_now_add=True)
