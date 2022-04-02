6005cc8f9ac4eb8171966ddedb316fcbb83e34fc
KostyaArent <trepalin.konstantin@gmail.com>
2022-04-02 23:57:42 +0000

    random fact, prog_lang added to template

--- personal_portfolio/portfolio/views.py
+++ personal_portfolio/portfolio/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Developer, Fact
+import random
 
 def home(request):
     projects = Project.objects.all()
-    return render(request, 'portfolio/home.html',{'projects':projects})
+    developer = Developer.objects.first()
+    languages = list(developer.languages.values_list('title', flat=True))
+    languages_str = ', '.join([language for language in languages])
+    facts = list(developer.facts.values_list('description', flat=True))
+    fact = facts[random.randint(0, len(facts)-1)]
+    return render(request, 'portfolio/home.html',{'projects':projects, 'languages_str': languages_str, 'fact':fact})
